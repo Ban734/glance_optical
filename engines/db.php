1f8f471bba268b539696945f18b0434c6e077bc2
Ban734 <sarcenochrisivan03@gmail.com>
2025-10-17 09:33:59 +0800

    Blah blah

--- engines/db.php
+++ engines/db.php
@@ -6,7 +6,12 @@ $dbname = "glance_db";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// Set timezone to Philippines
+date_default_timezone_set('Asia/Manila');
+$conn->query("SET time_zone = '+08:00'");
 ?>
--- engines/db_connection.php
+++ engines/db_connection.php
@@ -1,5 +1,15 @@
 <?php
 function connectDB() {
-    return new mysqli("localhost", "root", "", "glance_db");
+    $conn = new mysqli("localhost", "root", "", "glance_db");
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Set timezone to Philippines
+    date_default_timezone_set('Asia/Manila');
+    $conn->query("SET time_zone = '+08:00'");
+
+    return $conn;
 }
 ?>
